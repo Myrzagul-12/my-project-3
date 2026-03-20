1f8a68eee20a53a9eeda14ab23a3e26cf8170e5c
Jenishova Myrzagul <murzagulj@gmail.com>
2026-03-21 01:42:28 +0400

    Добавил форматирование даты и обновил файлы проекта

---
+++ .go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func FormatDate(t time.Time) string {
+	return t.Format("02.01.2006 15:04")
+}
+
+func main() {
+	now := time.Now()
+	fmt.Println(FormatDate(now)) 
+}
